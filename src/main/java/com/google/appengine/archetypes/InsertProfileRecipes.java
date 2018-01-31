9162b733fef6e13860279582d458ad7f6cda90cc
miller <miller.gutierrez1@gmail.com>
2018-01-31 15:33:41 +0100

    new file:   CAppCake 	new file:   src/main/java/com/google/appengine/archetypes/InsertProfileRecipes.java

---
+++ src/main/java/com/google/appengine/archetypes/InsertProfileRecipes.java
@@ -0,0 +1,47 @@
+package com.google.appengine.archetypes;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class InsertProfileRecipes
+ */
+@WebServlet("/insert_profile_recipes")
+public class InsertProfileRecipes extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+	
+	
+	DbConnection monitor;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public InsertProfileRecipes() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		response.setStatus(404);
+	
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+		
+		
+	}
+
+}
